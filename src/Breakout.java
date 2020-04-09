618542b07e5ab80856ce3560b8d2b26c9d33d50b
KKOULY <dmt.solovyov@gmail.com>
2020-04-09 14:22:35 +0300

    Create Bricks

--- src/Breakout.java
+++ src/Breakout.java
@@ -1,8 +1,8 @@
 /*
  * File: Breakout.java
  * -------------------
- * Name:
- * Section Leader:
+ * Name: Serdyuk Fedir & Soloviov Dmytro
+ * Section Leader: Pyechkurova Olena
  * 
  * This file will eventually implement the game of Breakout.
  */
@@ -56,11 +56,55 @@ public class Breakout extends GraphicsProgram {
 
 /** Number of turns */
 	private static final int NTURNS = 3;
+	
+	/** Number of lives */
+	private static final int LIVES = 2;
+	
 
 /* Method: run() */
 /** Runs the Breakout program. */
 	public void run() {
-		/* You fill this in, along with any subsidiary methods */
+		this.setSize(WIDTH+16,HEIGHT+62);
+		Color backgroundCol = new Color(48,48,48);
+		this.setBackground(backgroundCol);
+		drawBricks();
+
+	}
+
+	private Color returnCol (int num){
+			switch (num){
+				case 0: return Color.red;
+				case 1: return Color.orange;
+				case 2: return Color.yellow;
+				case 3: return Color.green;
+				case 4: return Color.cyan;
+				default: return null;
+		}
+	}
+
+	private void drawBricks() {
+		int yBrick = BRICK_Y_OFFSET;
+		for(int i = 0;i < NBRICK_ROWS;i++){
+			yBrick+=(BRICK_HEIGHT+BRICK_SEP);
+			int xBrick = BRICK_SEP;
+			for(int a = 0;a < NBRICKS_PER_ROW;a++) {
+				GRect rect = new GRect(BRICK_WIDTH, BRICK_HEIGHT);
+				rect.setFilled(true);
+				rect.setColor(nextColor(i));
+				add(rect,xBrick,yBrick);
+				xBrick+=(BRICK_WIDTH+BRICK_SEP);
+			}
+		}
+
+	}
+
+	private Color nextColor(int i) {
+		int numberOfColors = 5;
+		int boundaryNumberOfRows = NBRICK_ROWS/numberOfColors;
+		int ColorsNum = i/boundaryNumberOfRows;
+		return returnCol(ColorsNum);
+
+
 
 	}
 
