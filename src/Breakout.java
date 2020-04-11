370cc61cb14594f7a9265d4e04577b4cf9dca05b
KKOULY <dmt.solovyov@gmail.com>
2020-04-11 23:02:16 +0300

    Update Breakout.java

--- src/Breakout.java
+++ src/Breakout.java
@@ -268,7 +268,7 @@ public class Breakout extends GraphicsProgram {
 				pause(10);
 				remove(timerLabel);
 				timerLabel.setFont("Bahnschrift-"+i*3);
-				add(timerLabel,this.getWidth()/2-timerLabel.getWidth()/2,this.getHeight()-timerLabel.getHeight()/2);
+				add(timerLabel,this.getWidth()/2-timerLabel.getWidth()/2,this.getHeight()/2.0+timerLabel.getHeight()/2);
 				timerLabel.setLabel(""+time);
 			}
 			time--;
